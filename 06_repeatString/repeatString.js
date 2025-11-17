0c31374dca58c7bec94389bb0abdd8a3fead4186
declan-rs <declan.rsg@gmail.com>
2025-11-17 15:28:05 +1100

    Complete exercises 6-8

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0) {
+        return "ERROR";
+    }
 
+    let output = "";
+    for(i = 0; i < num; i++) {
+        output = output + str;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for(let i = 0; i < nums.length; i++) {
+        let toRemove = nums[i];
+        for(let j = 0; j < arr.length; j++) {
+            if(arr[j] === toRemove) {
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
