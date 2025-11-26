8dfec80ec97f565ce69893fd6c03986693cd2143
declan-rs <declan.rsg@gmail.com>
2025-11-26 21:12:58 +1100

    Complete exercises 9-11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(i, j) {
+    if(!Number.isInteger(i)||!Number.isInteger(j) || i < 0 || j < 0){
+        return "ERROR";
+    }
+    sum = 0;
+    small = 0;
+    large = 0;
+    if(j > i){
+        small = i;
+        large = j;
+    } else{
+        small = j;
+        large = i;
+    }
+    while(small<=large){
+        sum+=small;
+        small++;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
